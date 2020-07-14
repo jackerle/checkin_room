2ec4fb4d5608f96a639528c2f8b6e337d888c65d
jackerle <zeustololisis@gmail.com>
2020-07-14 19:52:34 +0700

    p. to cloud

--- api/repository_db/student_repo.js
+++ api/repository_db/student_repo.js
@@ -82,7 +82,7 @@ exports.checkin = function(room_id,u_id){
     values(${room_id},'${u_id}',CURRENT_TIMESTAMP,1)
     where not exists (
         select * from transaction where room_id =${room_id} and u_id = '${u_id} and status = 1'
-    )
+    ) limit 1
     ;` 
     return to_query(sql);
 }
@@ -343,4 +343,9 @@ exports.get_history = function (student_id,student_name,class_id,class_sect,star
     //     return to_query(sql+";")
     // }
     
+}
+
+
+exports.is_full = function(room_id){
+
 }
