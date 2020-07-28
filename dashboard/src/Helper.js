f6faf9803b4519d924af3aa4382efdfc4ff9ae94
jackerle <zeustololisis@gmail.com>
2020-07-28 17:33:12 +0700

    p. to cloud

--- dashboard/src/Helper.js
+++ dashboard/src/Helper.js
@@ -4,7 +4,7 @@ export function change_date_format (date){
     let _d = date
     let [y,m,d] = _d.split('T')[0].split('-')
     let [h,mi,s] = _d.split('T')[1].split(':')
-    return(h+":"+mi+" "+" "+d+"/"+m+"/"+y)
+    return(h+":"+mi+"น. "+" "+d+" "+change_month_format(m)+" "+(parseInt(y)+543))
 }
 
 export function change_day_format (day){
@@ -19,3 +19,43 @@ export function change_day_format (day){
 }
 
 
+function change_month_format (month){
+    switch(parseInt(month)){
+        case 1 :
+            return 'ม.ค.'
+            break
+        case 2 :
+            return 'ก.พ.'
+            break
+        case 3 :
+            return 'มี.ค.'
+            break
+        case 4 :
+            return 'เม.ย.'
+            break
+        case 5 :
+            return 'พ.ค.'
+            break
+        case 6 :
+            return 'มิ.ย.'
+            break
+        case 7 :
+            return 'ก.ค.'
+            break
+        case 8 :
+            return 'ส.ต.'
+            break
+        case 9 :
+            return 'ก.ย.'
+            break
+        case 10 :
+            return 'ต.ค.'
+            break
+        case 11 :
+            return 'พ.ย.'
+            break
+        case 12 :
+            return 'ธ.ค.'
+            break
+    }
+}
